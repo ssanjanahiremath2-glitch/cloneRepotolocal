b3920782f2de56063a7ef4a4bb256aea45a496c6
ssanjanahiremath2-glitch <ssanjanahiremath2@gmail.com>
2026-09-02 13:56:04 +0530

    Update userdetails.py

--- userdetails.py
+++ userdetails.py
@@ -1,4 +1,4 @@
 firstname = input("Enter your first name")
 lastname = input("Enter your last name")
 
-print("user first name and last name ",firstname)
+print("user first name and last name ",firstname + " "+lastname)
