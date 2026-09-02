9ecf2d4a0872552c8e16a006de18e008940d533b
Sanjana <ssanjanahiremath2@gmail.com>
2026-09-02 15:05:21 +0530

    modiefd

--- userdetails.py
+++ userdetails.py
@@ -1,4 +1,6 @@
 firstname = input("Enter your first name")
 lastname = input("Enter your last name")
+age = input("Enter a age")
 
 print("user first name and last name ",firstname + " "+lastname)
+print("user age ")
