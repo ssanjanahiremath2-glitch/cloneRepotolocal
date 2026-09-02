6c72fdeffbd2fdf1118ae9f58322208c7ce7cc46
ssanjanahiremath2-glitch <ssanjanahiremath2@gmail.com>
2026-09-02 14:37:51 +0530

    Update userdetails.py

--- userdetails.py
+++ userdetails.py
@@ -1,4 +1,9 @@
 firstname = input("Enter your first name")
 lastname = input("Enter your last name")
+college = input("Enter your college name")
+city = input("Enter your college city")
+contact = input("Enter your college contact number ")
 
 print("user first name and last name ",firstname + " "+lastname)
+print("college basic details ", college + "" +city)
+print("coolloge contact is",contact)
